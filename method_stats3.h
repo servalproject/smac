430259c9a2ffd461f0281f820b9cc3c306055fca
gardners <paul@servalproject.org>
2012-12-08 22:49:11 +1030

    fixed headers for per-character entropy logs.

--- method_stats3.h
+++ method_stats3.h
@@ -7,10 +7,11 @@ extern long long total_finalisation_bits;
 
 int stats3_compress(unsigned char *in,int inlen,unsigned char *out, int *outlen,
 		    stats_handle *h);
-int stats3_compress_bits(range_coder *c,unsigned char *m,int len,stats_handle *h);
+int stats3_compress_bits(range_coder *c,unsigned char *m,int len,stats_handle *h,
+			 double *entropyLog);
 int stats3_decompress(unsigned char *in,int inlen,unsigned char *out, int *outlen,
 		      stats_handle *h);
 int stats3_decompress_bits(range_coder *c,unsigned char m[1025],int *len_out,
-			   stats_handle *h);
+			   stats_handle *h,double *entropyLog);
 
 
