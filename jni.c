bdc678c9d6327b9241059386668468dba120320f
gardners <paul@servalproject.org>
2014-05-07 21:29:31 +0930

    fix xml2succinct() JNI function so that it works.

--- jni.c
+++ jni.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <jni.h>
+#include <android/log.h>
+ 
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "libsmac", __VA_ARGS__))
+
+#include "charset.h"
+#include "visualise.h"
+#include "arithmetic.h"
+#include "packed_stats.h"
+#include "smac.h"
+#include "recipe.h"
 
 JNIEXPORT jbyteArray JNICALL Java_org_servalproject_succinctdata_jni_xml2succinct
 (JNIEnv * env, jobject jobj,
@@ -23,15 +33,44 @@ JNIEXPORT jbyteArray JNICALL Java_org_servalproject_succinctdata_jni_xml2succinc
   int stripped_len=xml2stripped(recipefile,xmldata,strlen(xmldata),stripped,8192);
 
   if (stripped_len>0) {
+    // Produce succinct data
+
+    // Get stats handle
+    char filename[1024];
+    snprintf(filename,1024,"%s/smac.dat",path);
+    stats_handle *h=stats_new_handle(filename);
+
+    if (!h) {
+      LOGI("Could not read SMAC stats file %s",filename);
+      jbyteArray result=(*env)->NewByteArray(env, 3);
+      succinct[0]=99; succinct[1]=99; succinct[2]=2;
+      (*env)->SetByteArrayRegion(env, result, 0, 3, succinct);
+      return result;
+    }
+
+    // Read recipe file
+    snprintf(filename,1024,"%s/%s.recipe",path,recipefile);
+    struct recipe *recipe=recipe_read_from_file(filename);
+    
+    if (!recipe) {
+      LOGI("Could not read recipe file %s",filename);
+      jbyteArray result=(*env)->NewByteArray(env, 3);
+      succinct[0]=99; succinct[1]=99; succinct[2]=1;
+      (*env)->SetByteArrayRegion(env, result, 0, 3, succinct);
+      return result;
+    }
+
+    // Compress stripped data to form succinct data
+    succinct_len=recipe_compress(h,recipe,stripped,stripped_len,succinct,sizeof(succinct));
+    if (succinct_len<0) succinct_len=0;
+
+    // Clean up after ourselves
+    stats_handle_free(h);
+    recipe_free(recipe);
   }
   
-  // jbyteArray result=(*env)->NewByteArray(env, succinct_len);
-  // (*env)->SetByteArrayRegion(env, result, 0, succinct_len, succinct);
-
-  jbyteArray result=(*env)->NewByteArray(env, stripped_len);
-
-  (*env)->SetByteArrayRegion(env, result, 0, stripped_len, stripped);
-
+  jbyteArray result=(*env)->NewByteArray(env, succinct_len);
+  (*env)->SetByteArrayRegion(env, result, 0, succinct_len, succinct);
   
   return result;  
 }
