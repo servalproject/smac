8ad2d53fa19ac7f9af29a74049e933b5ed1e58a2
gardners <paul@servalproject.org>
2014-09-09 11:35:58 +0930

    move definitions to recipe.h

--- recipe.h
+++ recipe.h
@@ -1,3 +1,20 @@
+
+struct field {
+  char *name;
+  int type;
+  int minimum;
+  int maximum;
+  int precision; // meaning differs based on field type
+  char *enum_values[32];
+  int enum_count;
+};
+
+struct recipe {
+  struct field fields[1024];
+  int field_count;
+};
+
 int recipe_main(int argc,char *argv[],stats_handle *h);
+struct recipe *recipe_read_from_file(char *filename);
 
 extern char recipe_error[1024];
